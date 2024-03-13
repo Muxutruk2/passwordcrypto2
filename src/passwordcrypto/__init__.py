9628243416f53c5d38759e23fe565a7d54f172b3
Muxutruk2 <muxutruk2@users.noreply.github.com>
2024-03-13 17:57:59 +0100

    Copied discords doc format

--- src/passwordcrypto/__init__.py
+++ src/passwordcrypto/__init__.py
@@ -1,7 +1,1 @@
-"""
-This Python module provides a simple password manager for securely storing and retrieving encrypted password entries. 
-The passwords are encrypted using the Fernet symmetric encryption scheme. 
-The module includes functionality to derive a Fernet key from a password, initialize a password manager instance, retrieve encrypted passwords, decrypt and read stored passwords, encrypt text, and write new password entries to a file.
-"""
-__author__ = "Muxutruk"
-from src.passwordcrypto import *
+from passwordcrypto.passwordcrypto import Passwd
