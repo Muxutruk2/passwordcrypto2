afc50ec636c8db0f0b621dfad7e9f5edfd0cfde6
BenatEiguren <benat.eigurenzu@elorrieta-errekamari.com>
2024-03-13 16:17:18 +0100

    renamed module folder to src

--- src/__init__.py
+++ src/__init__.py
@@ -4,4 +4,4 @@ The passwords are encrypted using the Fernet symmetric encryption scheme.
 The module includes functionality to derive a Fernet key from a password, initialize a password manager instance, retrieve encrypted passwords, decrypt and read stored passwords, encrypt text, and write new password entries to a file.
 """
 __author__ = "Muxutruk"
-from passwordcrypto.passwordcrypto import *
+from src.passwordcrypto import *
