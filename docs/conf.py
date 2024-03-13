2f70cc83010dc5e337c9501ce49a9a8776793ec4
BenatEiguren <benat.eigurenzu@elorrieta-errekamari.com>
2024-03-13 19:45:56 +0100

    Fixed styling

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,9 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_style = "_static/custom.css"
 
 autodoc_default_options = {
     "members": True,
